746ea79a3457865121be6001e9d315368555396c
josecacciatore89 <60232884+josecacciatore89@users.noreply.github.com>
2021-06-19 14:04:58 -0300

    Add files via upload

---
+++ pessoa.cpp
@@ -0,0 +1,44 @@
+#include "pessoa.hpp"
+
+
+
+void Pessoa::pessoa()
+{
+    
+}
+
+void Pessoa::setLado(string n)
+{
+    nome = n;
+}
+
+void Pessoa::setEndereco(string e)
+{
+    endereco = e;
+}
+
+void Pessoa::setTelefone(string t)
+{
+    telefone = t;
+}
+
+void Pessoa::setEmail(string y)
+{
+    email = y;
+}
+
+void Pessoa::setCpf(string z)
+{
+    cpf = z;
+}
+
+void Pessoa::setCargo(string q)
+{
+    cargo = q;
+}
+
+
+void Pessoa::toString()
+{
+
+}
